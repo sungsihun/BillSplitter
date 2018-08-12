6873a68f5894345c03329e79d351b85aec6a0995
SiHun Sung <sungsihun@gmail.com>
2018-08-12 11:53:40 -0400

    final

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *splitBillAmountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeopleLabel;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
+@property (nonatomic) NSNumber *billAmount;
+@property (nonatomic) NSNumber *numberOfPeople;
+@property (nonatomic) NSNumberFormatter *numberFormatter;
 
 @end
 
@@ -16,13 +23,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.numberFormatter = [[NSNumberFormatter alloc] init];
+    [self.numberFormatter setFormatterBehavior:NSNumberFormatterBehaviorDefault];
+    [self.numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    
+    [self setupBillAmountTextField];
+    self.numberOfPeople = [NSNumber numberWithFloat:self.numberOfPeopleSlider.value];
+    self.numberOfPeopleLabel.text = [NSString stringWithFormat:@"Number of People: %@", self.numberOfPeople];
+}
+
+- (void)setupBillAmountTextField {
+    self.billAmountTextField.delegate = self;
+    self.billAmountTextField.keyboardType = UIKeyboardTypeNumberPad;
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)splitBillButton:(id)sender {
+    [self.billAmountTextField resignFirstResponder];
+    
+    self.billAmount = [self.numberFormatter numberFromString:self.billAmountTextField.text];
+    self.splitBillAmountLabel.text = [NSString stringWithFormat:@"split bill amout: $ %@", [self splitBillAmount]];
+    
+    
+    [self.numberFormatter stringFromNumber:[self splitBillAmount]];
+}
+
+- (NSNumber *)splitBillAmount {
+    return [NSNumber numberWithFloat:(self.billAmount.floatValue / self.numberOfPeople.floatValue)];
+}
+
+
+- (IBAction)sliderValueChanged:(id)sender {
+    self.numberOfPeople = [NSNumber numberWithInteger:self.numberOfPeopleSlider.value];
+    self.numberOfPeopleLabel.text = [NSString stringWithFormat:@"Number of People: %@", self.numberOfPeople];
 }
 
 
