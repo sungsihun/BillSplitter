5b85b1ce0908860e833582c89db4397ade7238f6
SiHun Sung <sungsihun@gmail.com>
2018-08-12 10:17:41 -0400

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by NICE on 2018-08-12.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by NICE on 2018-08-12.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
